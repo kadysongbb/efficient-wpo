7f998495aff09d237cf26f832c95c7ce26df3e06
kadysongbb <kadysongbb@gmail.com>
2021-01-11 02:46:57 -0800

    update

--- tabular/gym_gridworld/envs/GridWorld.py
+++ tabular/gym_gridworld/envs/GridWorld.py
@@ -14,7 +14,7 @@ LEFT = 3
 class GridWorld(gym.Env):
     metadata = {'render.modes': ['human']}
 
-    def __init__(self, file_name = "yellow_room1.txt", fail_rate = 0.0, terminal_reward = 100.0, move_reward = -1, bump_reward = -5, bomb_reward = -10):
+    def __init__(self, file_name = "yellow_room1.txt", fail_rate = 0.0, terminal_reward = 100.0, move_reward = -1, bump_reward = -1, bomb_reward = -10):
         self.viewer = SimpleImageViewer()
         self.n = None
         self.m = None
@@ -112,8 +112,6 @@ class GridWorld(gym.Env):
         if new_state in self.goals:
             self.done = True
             return self.terminal_reward
-        elif new_state in self.bombs:
+        if new_state in self.bombs:
             return self.bomb_reward
-        elif new_state == self.state:
-            return self.bump_reward
         return self.move_reward
