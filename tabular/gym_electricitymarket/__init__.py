c7fc5038e6240d90d024eb1a08a73990295579b1
kadysongbb <kadysongbb@gmail.com>
2021-03-26 00:35:22 -0700

    update

--- tabular/gym_electricitymarket/__init__.py
+++ tabular/gym_electricitymarket/__init__.py
@@ -4,3 +4,13 @@ register(
     id='ElectricityMarket-v0',
     entry_point='gym_electricitymarket.envs:ElectricityMarket',
 )
+
+register(
+    id='ElectricityMarketDiscrete-v0',
+    entry_point='gym_electricitymarket.envs:ElectricityMarketDiscrete',
+)
+
+register(
+    id='ElectricityMarketDiscreteDQN-v0',
+    entry_point='gym_electricitymarket.envs:ElectricityMarketDiscreteDQN',
+)
